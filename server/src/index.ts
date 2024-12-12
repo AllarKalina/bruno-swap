086f2b6bf66d3ee385b3fc26e1d032c46786408f
Allar Kalina <allarkalina@gmail.com>
2024-12-12 09:34:33 +0200

    Remove accidental node_modules

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,12 @@
 import express from "express";
 import http from "http";
 import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
 import compression from "compression";
-import cors from "cors";
 import tokenRouter from "token/token-routes.js";
 
 const app = express();
 
-app.use(cors({ credentials: true }));
 app.use(compression());
-app.use(cookieParser());
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
